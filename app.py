526ce747351dc3e0847f7af23596c428ae0cd0af
chrisgmez1 <57599092+chrisgmez1@users.noreply.github.com>
2020-12-05 14:08:53 -0600

    added stuff
    added json with student info, added functions to allow buttons to view user/admin view

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
 import tkinter as tk
 from tkinter import *
 
+
+
+def admin_view():
+    admin_screen = tk.Tk()
+    admin_screen.title("Admin View")
+    admin_screen.geometry("750x600")
+    Label(admin_screen,text = "Admin",bg = "red", height ="2", width="750").pack()
+    canvas_adminview = tk.Canvas(admin_screen,bg="grey",width ="650",height="500")
+    canvas_adminview.pack()
+
+    adminLog = tk.Button(admin_screen, text = "Change Grades", padx = 10, pady = 5
+                                        ,fg="black")
+    adminLog.pack()
+
+    admin_screen.mainloop()
+def user_view():
+    user_screen = tk.Tk()
+    user_screen.title("Admin View")
+    user_screen.geometry("750x600")
+    Label(user_screen,text = "User",bg = "blue", height ="2", width="750").pack()
+    canvas_userview = tk.Canvas(user_screen,bg="grey",width ="650",height="500")
+    canvas_userview.pack()
+
+    userLog = tk.Button(user_screen, text = "Change Grades", padx = 10, pady = 5
+                                        ,fg="black")
+    userLog.pack()
+
+    user_screen.mainloop()
 def main_screen():
     screen = tk.Tk()
     screen.title("Learning Managment System")
     screen.geometry("300x250")
     Label(text = "Login",bg = "grey", height ="2", width="250").pack()
+    
     adminLog = tk.Button(screen, text = "Admin Login", padx = 10, pady = 5
-                                        ,fg="black")
+                                        ,fg="black", command =admin_view)
     adminLog.pack()
 
     userLog = tk.Button(screen, text = "User Login", padx = 10, pady = 5
-                                        ,fg="black")
+                                        ,fg="black", command= user_view)
     userLog.pack()
 
     screen.mainloop()
